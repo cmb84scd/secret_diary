d5bc4c895ac83da5785659f9c715a75b00660d5d
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-21 14:14:28 +0100

    split one class into two but still in one file

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,9 +1,27 @@
 class SecretDiary
   attr_reader :security, :entries
 
-  def initialize
-    @security = "locked"
+  def initialize(security = Locked)
+    @security = security.new
     @entries = []
+  end
+
+  def add_entry(entry)
+    fail "Diary is locked, unable to add entry" if security.locked?
+
+    @entries << entry
+  end
+
+  def get_entries
+    fail "Diary is locked, unable to get entries" if security.locked?
+
+    @entries
+  end
+end
+
+class Locked
+  def initialize
+    @locked = "locked"
   end
 
   def unlock
@@ -14,20 +32,7 @@ class SecretDiary
     @security = "lock"
   end
 
-  def add_entry(entry)
-    fail "Diary is locked, unable to add entry" if locked?
-
-    @entries << entry
-  end
-
-  def get_entries
-    fail "Diary is locked, unable to get entries" if locked?
-
-    @entries
-  end
-
-  private
   def locked?
-    @security == "locked"
+    @locked == "locked"
   end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,18 +1,6 @@
 require 'secret_diary'
 
 describe SecretDiary do
-  describe 'unlock' do
-    it 'unlocks the diary' do
-      expect(subject.unlock).to eq 'unlock'
-    end
-  end
-
-  describe 'lock' do
-    it 'locks the diary' do
-      expect(subject.lock).to eq 'lock'
-    end
-  end
-
   describe 'add_entry' do
     it 'raises an error if diary is locked' do
       expect { subject.add_entry 1 }.to raise_error("Diary is locked, unable to add entry")
@@ -22,6 +10,20 @@ describe SecretDiary do
   describe 'get_entries' do
     it 'raises an error if diary is locked' do
       expect { subject.get_entries }.to raise_error("Diary is locked, unable to get entries")
+    end
+  end
+end
+
+describe Locked do
+  describe 'unlock' do
+    it 'unlocks the diary' do
+      expect(subject.unlock).to eq 'unlock'
+    end
+  end
+
+  describe 'lock' do
+    it 'locks the diary' do
+      expect(subject.lock).to eq 'lock'
     end
   end
 end
